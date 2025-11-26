4c3e7720a1c7029c49781bbafe482c267b53085a
Keisuke Takahashi <genspark_dev@genspark.ai>
2025-11-26 04:44:29 +0000

    chore: 不要なファイルを削除してシンプル化
    - React/Vite関連ファイルを完全削除
    - node_modules, dist, src, publicフォルダを削除
    - package.json等のnpm関連ファイルを削除
    - Vanilla JavaScriptのみのシンプル構成に
    - README.mdを追加
    - .gitignoreを更新

--- background.js
+++
@@ -1,7 +0,0 @@
-// background.js
-// アイコンクリック時にビューワーを開く
-
-chrome.action.onClicked.addListener(async (tab) => {
-    const viewerUrl = chrome.runtime.getURL("index.html");
-    await chrome.tabs.create({ url: viewerUrl });
-});
